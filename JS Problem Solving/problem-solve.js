986af62e75157e0aad637ce09c095481f69882f4
jahidhowlader <jahidhowlader.info@gmail.com>
2023-02-04 10:47:51 +0600

    add new code for solving problem

--- JS Problem Solving/problem-solve.js
+++ JS Problem Solving/problem-solve.js
@@ -1,12 +1,24 @@
-function countTrue(arr) {
-	let result = 0
-    for( let i = 0; i < arr.length; i++){
-        if(arr[i] === true){
-            result += 1
-        }
-    }
-    return result
-}
+// function countTrue(arr) {
+// 	let result = 0
+//     for( let i = 0; i < arr.length; i++){
+//         if(arr[i] === true){
+//             result += 1
+//         }
+//     }
+//     return result
+// }
 
-const result = countTrue([true, true, false, false, false, false, true, false, true, true, false, true])
-console.log(result);
+// const result = countTrue([true, true, false, false, false, false, true, false, true, true, false, true])
+// console.log(result);
+
+// function toArray(object){
+//     let arr = []
+//     for(let i in object){
+//         arr.push([i, object[i]])
+//     }
+//     return arr
+// }
+
+// const result = toArray({})
+// console.log(result);
+
