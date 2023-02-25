49c53ebb1e5f3939ebca604c58772b88a29ceb06
jahidhowlader <jahidhowlader.info@gmail.com>
2023-02-25 18:29:07 +0600

    updat code

--- Mpdule-34 [API, JSON, Data Load]/script/countries.js
+++ Mpdule-34 [API, JSON, Data Load]/script/countries.js
@@ -1,54 +1,41 @@
-const loadCountries = _ => {
+const loadAllCountryApi = _ => {
     fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
-        .then(data => displayCountries(data))
+        .then(data => displayAllCountry(data))
 }
 
-const displayCountries = country => {
-    const container = document.getElementById('countries')
-
-    country.forEach(data => {
-        const element = document.createElement('div')
-        element.innerHTML = `
-        <h2><strong>Country Name:</strong> ${data.name.common}</h2>
-        <h3><strong>Capital:</strong> ${data.capital}</h3>
-        <button onclick="countriesDetails('${data.cca2}')" class="py-2 px-4 bg-red-400 rounded mt-2">Details</button>
+const displayAllCountry = countries => {
+    const allCountryElement = document.getElementById('countries')
+    countries.forEach(country => {
+        const singleCountryElement = document.createElement('div')
+        singleCountryElement.innerHTML = `
+        <h2><strong class="text-red-500">Country Name:</strong> ${country.name.common}</h2>
+        <h3><strong class="text-green-500">Capital Name:</strong> ${country.capital}</h3>
+        <button onclick="loadSingleCountry('${country.cca2}')" class="bg-yellow-400 mt-2 py-2 px-5 rounded">Details<button/>
         `
-        element.classList.add('border', 'p-2', 'rounded-lg')
-        container.appendChild(element)
-
-        // console.log(data.cca2);
-
+        singleCountryElement.classList.add('border', 'p-2', 'rounded')
+        allCountryElement.appendChild(singleCountryElement)
     })
 }
 
-const countriesDetails = data => {
-    const URL = `https://restcountries.com/v3.1/alpha/${data}`
+const loadSingleCountry = code => {
+    const URL = `https://restcountries.com/v3.1/alpha/${code}`
 
     fetch(URL)
-    .then(res => res.json())
-    .then(data => displayCountryDetails(data))
+        .then(res => res.json())
+        .then(data => displayCountry(data[0]))
 }
 
-const displayCountryDetails = details => {
+const displayCountry = country => {
     const container = document.getElementById('countries-details')
-    // const element = document.createElement('div')
-        container.innerHTML =   `
-        <div>
-        <h2><strong>Country Name:</strong> ${details[0].name.common}</h2>
-        <h3><strong>Capital:</strong> ${details[0].capital}</h3>
-        <h3><strong>Code:</strong> ${details[0].cca2}</h3>
-        </div>
-        `
+    container.innerHTML = `
+    <div>
+    <h2><strong class="text-red-500">Country Name:</strong> ${country.name.common}</h2>
+    <h3><strong class="text-green-500">Capital Name:</strong> ${country.capital}</h3>
+    <img src="${country.flags.png}">
 
-        // container.appendChild(element)
-    // console.log(element);
-    // console.log('data');
-    // const chilElement = document.createElement('div')
-    // chilElement.innerHTML = `
-    // <h2>Country Name: ${data.name.common}</h2>
-    // `
-    // container.appendChild(chilElement)
+    </div>
+    `
 }
 
-loadCountries()
+loadAllCountryApi()
